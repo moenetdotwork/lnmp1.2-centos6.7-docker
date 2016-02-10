08094095639e89d2aca0db314f864f82b1eff095
Max <maxwhale@users.noreply.github.com>
2016-02-10 23:54:00 +0800

    Update main-centos.sh

--- main-centos.sh
+++ main-centos.sh
@@ -191,8 +191,23 @@ Apache_Selection()
 #     stty -icanon -echo min 1 time 0
 #     dd count=1 2>/dev/null
 #     stty ${OLDCONFIG}
+#    . include/version.sh
+# }
+get_char()
+    {
+    SAVEDSTTY=`stty -g`
+    stty -echo
+    stty cbreak
+    dd if=/dev/tty bs=1 count=1 2> /dev/null
+    stty -raw
+    stty echo
+    stty $SAVEDSTTY
     . include/version.sh
-#}
+    }
+    echo ""
+    echo "Press any key to start...or Press Ctrl+c to cancel"
+    #char=`get_char`
+  
 
 Press_Start()
 {
