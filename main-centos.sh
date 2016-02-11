a845296049bdd03fbaf4f320e9c2ccd0f4e2b2fa
Max <maxwhale@users.noreply.github.com>
2016-02-11 12:22:09 +0800

    Update main-centos.sh

--- main-centos.sh
+++ main-centos.sh
@@ -183,31 +183,13 @@ Apache_Selection()
     fi
 }
 
-#Press_Install()
-# {
-#    echo ""
-#    echo "Press any key to install...or Press Ctrl+c to cancel"
-#     OLDCONFIG=`stty -g`
-#     stty -icanon -echo min 1 time 0
-#     dd count=1 2>/dev/null
-#     stty ${OLDCONFIG}
-#    . include/version.sh
-# }
-get_char()
-    {
-    SAVEDSTTY=`stty -g`
-    stty -echo
-    stty cbreak
-    dd if=/dev/tty bs=1 count=1 2> /dev/null
-    stty -raw
-    stty echo
-    stty $SAVEDSTTY
+
+    OLDCONFIG=`stty -g`
+    stty -icanon -echo min 1 time 0
+    #dd count=1 2>/dev/null
+    stty ${OLDCONFIG}
     . include/version.sh
-    }
-    echo ""
-    echo "Press any key to start...or Press Ctrl+c to cancel"
-    #char=`get_char`
-  
+
 
 Press_Start()
 {
